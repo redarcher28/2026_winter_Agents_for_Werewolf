09fc2292cdf88e0f77888f427dfe487d54ae61d8
redarcher28 <123471396+redarcher28@users.noreply.github.com>
2026-01-04 21:11:55 +0800

    develop (#2)
    * 组长的第一次提交测试
    
    Signed-off-by: redarcher28 <3163929235@qq.com>
    
    * 组长的第一次提交测试
    
    Signed-off-by: redarcher28 <3163929235@qq.com>
    
    * 组长的第一次提交测试
    
    Signed-off-by: redarcher28 <3163929235@qq.com>
    
    * 组长的第三次提交测试
    
    Signed-off-by: redarcher28 <3163929235@qq.com>
    
    ---------
    
    Signed-off-by: redarcher28 <3163929235@qq.com>

---
+++ test.py
@@ -0,0 +1,12 @@
+def decorator1(func):
+    print("decorator1")
+    return func
+def decorator2(func):
+    print("decorator2")
+    return func
+@decorator1
+@decorator2
+def foo():
+    pass
+foo()
+print("world")
