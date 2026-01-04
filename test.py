7e3bbbe08b508484901c798c8ec0114fd2842a42
redarcher28 <3163929235@qq.com>
2026-01-04 20:10:19 +0800

    组长的第一次提交测试
    Signed-off-by: redarcher28 <3163929235@qq.com>

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("test")
+def decorator1(func):
+    print("decorator1")
+    return func
+def decorator2(func):
+    print("decorator2")
+    return func
+@decorator1
+@decorator2
+def foo():
+    pass
+foo()
+
+print(foo)
+def decorator3(func):
+    print("decorator3")
+    return func
